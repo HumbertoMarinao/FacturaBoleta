c841b7376858ce0b9deae6a90af10a52a499a1b4
humberto geovanni marinao vidal <humberto.mv@gmail.com>
2019-09-03 12:28:57 -0400

    Release conectado con develop y master

---
+++ SisBolFactu/Data/SisBolFactuContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SisBolFactu.Models
+{
+    public class SisBolFactuContext : DbContext
+    {
+        public SisBolFactuContext (DbContextOptions<SisBolFactuContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SisBolFactu.Models.Categoria> Categoria { get; set; }
+    }
+}
--- SisBolFactu/Startup.cs
+++ SisBolFactu/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SisBolFactu.Models;
 
 namespace SisBolFactu
 {
@@ -33,6 +35,9 @@ namespace SisBolFactu
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SisBolFactuContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SisBolFactuContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
